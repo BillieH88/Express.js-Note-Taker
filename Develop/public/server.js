80bfed9cf3fab982b46a2dbc1c41d74d50f7b5dc
Billie88 <bhussein088@gmail.com>
2021-05-09 16:33:02 -0400

    added fs module/path module/express module

---
+++ Develop/public/server.js
@@ -0,0 +1,17 @@
+//Require file system module
+const fs = require('fs');
+// Require path module
+const path = require('path');
+// REQUIRE in the express Library
+const express = require('express');
+// Instantiate an express server
+const app = express();
+// Declare port value 
+const PORT = 4500;
+
+
+
+// app.get('./db/db.json',(req,res) =>{
+// console.log('./db/db.json');
+// })
+
