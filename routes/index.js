7bafaa62d872fb4e90398fbc788426d9494a4a87
Billie88 <bhussein088@gmail.com>
2021-05-10 17:43:04 -0400

    added both get and post functionality to note taker

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const router = require("express").Router();
-// response get is from notes html page
+
+
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
-// response get is from index html page
-router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+
+
+
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const fs = require("fs");
+const path = require('path');
 // Instantiate an express server
 const app = express();
 // Declare port value
 const PORT = 6200;
-const notes = require("./db/db.json");
-const routes = require("./routes");
+
+const routes= require("./routes");
+var notes = require("./db/db.json")
+
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -12,6 +16,26 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", routes);
 
+app.get("/api/notes", (req, res)=> {
+  console.log(notes);
+      res.status(200).send(
+          notes
+      )
+  })
+
+app.post('/api/notes', (req,res) =>{
+  const data = req.body;
+  let newId = notes[notes.length -1].id +1;
+  const allNewNotes = notes.concat({id: newId, ...data});
+  notes = allNewNotes
+  return res.status(201).send(allNewNotes);
+});
+
+app.all("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log("Listening for PORT on " + PORT);
 });
+
